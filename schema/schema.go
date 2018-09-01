863a7b01f065847a66a8b6de866c421503da2f77
cabbagen <754689243@qq.com>
2018-09-01 14:10:45 +0800

    调整缩进

--- schema/schema.go
+++ schema/schema.go
@@ -5,6 +5,6 @@ import "time"
 type Schema struct {
   ID          uint         `gorm:"primary_key" json:"id"`
   CreatedAt   time.Time    `json:"createdAt"`
-  UpdatedAt   time.Time		 `json:"updatedAt"`
+  UpdatedAt   time.Time    `json:"updatedAt"`
   DeletedAt   *time.Time   `sql:"index" json:"deletedAt"`      
 }
