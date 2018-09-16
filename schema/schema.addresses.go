e618632aaae1bf7b4f157f10f74e93c1bb4b7772
cabbagen <754689243@qq.com>
2018-09-16 14:39:28 +0800

    转换文件

--- schema/schema.addresses.go
+++ schema/schema.addresses.go
@@ -1,9 +1,9 @@
 package schema
 
 type Address struct {
-	Schema
-	Name         string             `gorm:"column:name;type=varchar(255);not null" json:"name"`
-	Code         string             `gorm:"column:code;type=varchar(120);not null;unique" json:"code"`
-	Level        int8               `gorm:"column:level;type=tinyint;not null" json:"level"`
-	ParentCode   string             `gorm:"column:parent_code;type=varchar(120);not null" json:"parentCode"`
+  Schema
+  Name         string             `gorm:"column:name;type=varchar(255);not null" json:"name"`
+  Code         string             `gorm:"column:code;type=varchar(120);not null;unique" json:"code"`
+  Level        int8               `gorm:"column:level;type=tinyint;not null" json:"level"`
+  ParentCode   string             `gorm:"column:parent_code;type=varchar(120);not null" json:"parentCode"`
 }
--- schema/schema.channels.go
+++ schema/schema.channels.go
@@ -1,7 +1,7 @@
 package schema
 
 type Channel struct {
-	Schema
-	Name         string             `gorm:"column:name;type=varchar(255);not null;unique" json:"name"`
-	State        int8               `gorm:"column:state;type=tinyint;not null" json:"state"`
+  Schema
+  Name         string             `gorm:"column:name;type=varchar(255);not null;unique" json:"name"`
+  State        int8               `gorm:"column:state;type=tinyint;not null" json:"state"`
 }
