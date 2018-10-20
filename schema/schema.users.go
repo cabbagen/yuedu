ceab0327bda8c4ab380d55315a2491038816e39d
cabbagen <754689243@qq.com>
2018-10-20 10:44:06 +0800

    修改数据库字段

--- schema/schema.users.go
+++ schema/schema.users.go
@@ -7,7 +7,7 @@ type User struct {
   Gender           int8         `gorm:"column:gender;type=tinyint;not null" json:"gender"`
   Email            string       `gorm:"column:email;type=varchar(255);not null;default:\"\";unique" json:"email"`
   Address          int          `gorm:"column:address;type=int;not null" json:"address"`
-  HomePage         string       `gorm:"column:homepage;type=varchar(255);not null;default:\"\"" json:"homepage"`
+  HomePages        string       `gorm:"column:homepages;type=varchar(255);not null;default:\"\"" json:"homepages"`
   Avator           string       `gorm:"column:avator;type=varchar(255);not null;default:\"\"" json:"avator"`
   Backdrop         string       `gorm:"column:backdrop;type=varchar(255);not null;default:\"\"" json:"backdrop"`
   Extra            string       `gorm:"column:extra;type=varchar(255);not null;default:\"\"" json:"extra"`
